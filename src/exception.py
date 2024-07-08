4a08d8ec2d9ac2eea525ce0369b340bff3d89f0e
nihartripathi <nihartripathi00@gmail.com>
2024-07-08 13:11:21 -0400

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys 
-import logging
+from logger import logging
+
 
 def error_message_detail(error,error_detail):
     _,_,exc_tb=error_detail.exc_info()
@@ -11,7 +12,7 @@ def error_message_detail(error,error_detail):
     return error_message
     
 
-class CustonException(Exception):
+class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message,error_detail=error_detail)
@@ -19,3 +20,9 @@ class CustonException(Exception):
     def __str__(self):
         return self.error_message
 
+if __name__ == "__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divided by zero")
+        raise CustomException(e,sys)
